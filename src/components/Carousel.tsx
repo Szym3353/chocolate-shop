737fa7402164ae77106c665c342d2eca6e0c5e32
szym3353 <nielo7078@gmail.com>
2024-03-05 19:09:28 +0100

    Updated carousel component to be more customizable

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,37 +4,61 @@ import "../css/carousel.css";
 export default function Carousel<T>({
   data,
   Component,
+  inView = 4,
 }: {
   data: T[];
   Component: React.FC<any>;
+  inView?: number;
 }) {
+  let [currentInView, setCurrentInView] = React.useState(inView);
+  let [elementWidth, setElementWidth] = React.useState<number>(300);
   let containerRef = React.useRef<HTMLDivElement | null>(null);
+  let padding = 20;
+  let margin = 20;
+  let totalGap = (currentInView - 1) * 20;
 
   React.useEffect(() => {
     if (!containerRef.current) return;
-    containerRef.current.scrollLeft = containerRef.current.scrollWidth / 2;
+
+    setElementWidth(
+      (containerRef.current.offsetWidth - padding - totalGap) / currentInView
+    );
 
     containerRef.current.addEventListener("scroll", () => {
       if (!containerRef.current) return;
+
       if (containerRef.current.scrollLeft <= 0) {
         containerRef.current.scrollLeft = containerRef.current.scrollWidth / 2;
       } else if (
         containerRef.current.scrollLeft >=
         containerRef.current.scrollWidth - containerRef.current.offsetWidth - 10
       ) {
-        containerRef.current.scrollLeft = 272.5;
+        containerRef.current.scrollLeft =
+          containerRef.current.scrollWidth / 2 -
+          4 * (elementWidth + padding + margin);
       }
     });
   }, []);
 
+  React.useEffect(() => {
+    if (!containerRef.current) return;
+    containerRef.current.scrollLeft = containerRef.current.scrollWidth / 2;
+  }, [elementWidth]);
+
   function scrollLeft() {
     if (!containerRef.current) return;
-    containerRef.current.scrollBy({ left: -272.5, behavior: "smooth" });
+    containerRef.current.scrollBy({
+      left: -(elementWidth + padding),
+      behavior: "smooth",
+    });
   }
 
   function scrollRight() {
     if (!containerRef.current) return;
-    containerRef.current.scrollBy({ left: 272.5, behavior: "smooth" });
+    containerRef.current.scrollBy({
+      left: elementWidth + padding,
+      behavior: "smooth",
+    });
   }
 
   return (
@@ -42,7 +66,11 @@ export default function Carousel<T>({
       <button onClick={scrollLeft}>Prev</button>
       <div ref={containerRef} className="carousel-inner">
         {data.map((item, index) => (
-          <div key={index} className="carousel-item">
+          <div
+            key={index}
+            className="carousel-item"
+            style={{ minWidth: elementWidth }}
+          >
             <Component {...item} />
           </div>
         ))}
