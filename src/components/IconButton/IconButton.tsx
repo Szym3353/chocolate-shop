836c9a7f04f6d62c6dcb65102624910b4d00cdd2
szym3353 <nielo7078@gmail.com>
2024-03-12 11:22:18 +0100

    Added responsivity to navbar and updated icon button - added onclick and additional styles to icon button component - navbar now change while in mobile view

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,19 +1,28 @@
 // favorite
 
+import React from "react";
 import "./icon-button.css";
 
 interface PropsI {
   icon: string;
   variant?: "full" | "outlined";
+  onClick?: () => void;
+  style?: React.CSSProperties;
 }
 
-export default function IconButton({ variant = "full", icon }: PropsI) {
+export default function IconButton({
+  variant = "full",
+  icon,
+  onClick,
+  style,
+}: PropsI) {
   return (
-    <button className="icon-button">
+    <button className="icon-button" onClick={onClick}>
       <span
         className={`material-symbols-${
           variant === "full" ? "sharp" : "outlined"
         }`}
+        style={{ ...style }}
       >
         {icon}
       </span>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import React from "react";
 import "../css/navbar.css";
 import IconButton from "./IconButton/IconButton";
 import Logo from "./Logo";
 
 export default function Navbar() {
+  let navRef = React.useRef<HTMLDivElement | null>(null);
+
+  const handleToggle = () => {
+    if (!navRef.current) return;
+    let computedStyle = window.getComputedStyle(navRef.current);
+    let currentLeft = computedStyle.getPropertyValue("left");
+    if (currentLeft[0] === "-") {
+      navRef.current.style.left = "0px";
+    } else {
+      navRef.current.style.left = "-300px";
+    }
+  };
+
   return (
     <nav className="nav">
       <div className="navbar__row">
@@ -12,8 +26,14 @@ export default function Navbar() {
           <IconButton icon="favorite" />
           <IconButton icon="person" />
         </div>
+        <div className="visibleMobile">
+          <IconButton icon="menu" onClick={handleToggle} />
+        </div>
       </div>
-      <div className="navbar__row">
+      <div className="navbar__row navbar__menu" ref={navRef}>
+        <div className="visibleMobile">
+          <Logo />
+        </div>
         <div className="navbar__col">
           <a href="#" className="navbar__link">
             Produkty
@@ -32,6 +52,20 @@ export default function Navbar() {
           <a href="#" className="navbar__link">
             O nas
           </a>
+        </div>
+        <div className="visibleMobile">
+          <div className="navbar__icons--mobile">
+            <IconButton icon="shopping_cart" style={{ fontSize: 30 }} />
+            <IconButton icon="favorite" style={{ fontSize: 30 }} />
+            <IconButton icon="person" style={{ fontSize: 30 }} />
+          </div>
+        </div>
+        <div className="visibleMobile">
+          <IconButton
+            icon="cancel"
+            style={{ fontSize: 30 }}
+            onClick={handleToggle}
+          />
         </div>
       </div>
     </nav>
